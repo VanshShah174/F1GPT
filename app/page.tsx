5169c1459873855916308783dd6b525c00104284
vansh.shah <vanshshah174@gmail.com>
2025-06-11 17:28:29 -0400

    UI modification completed along with code changes in the page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,19 @@
 import Image from "next/image";
 import f1GPTLogo from "./assets/F1GPT.png"
 import {useChat} from "@ai-sdk/react";
-import { Message } from "ai";
+import {Message} from "ai";
 
 const Home = () => {
 
-    const noMessages = false
+    const {append, status , messages, input, handleInputChange, handleSubmit} = useChat();
+
+
+    const noMessages = true
 
     return (
         <main>
-            <Image src={f1GPTLogo} alt="f1GPTLogo" width="250"  />
-            <section>
+            <Image src={f1GPTLogo} alt="f1GPTLogo" width="250"/>
+            <section className={noMessages ? " " : "populated"}>
                 {noMessages ? (
                     <>
                         <p className="starter-text">
@@ -21,7 +24,7 @@ const Home = () => {
                             and it will come back with the most up-to-date answers.
                             We hope you enjoy!!
                         </p>
-                        <br />
+                        <br/>
                         {/*<PromptSuggestionRow />*/}
                     </>
                 ) : (
@@ -29,8 +32,16 @@ const Home = () => {
                         {/*map messages onto text bubbles*/}
                         {/*<LoadingBubble />*/}
                     </>
-                ) }
+                )}
+
+
             </section>
+                <form onSubmit={handleSubmit}>
+                    <input className="question-box" onChange={handleInputChange} value={input}
+                           placeholder="Ask me something... "/>
+                    <input type="submit"/>
+                </form>
+
         </main>
     )
 }
